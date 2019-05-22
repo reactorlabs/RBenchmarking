fdeb393f7c06580f8fc3a60af802f17d09716fe0
Guido Chari <charig@gmail.com>
2019-05-22 15:01:21 +0200

    ..

--- RunScripts/runAndPublish.sh
+++ RunScripts/runAndPublish.sh
@@ -36,15 +36,16 @@ TIMESTAMP=$(timestamp)
 pushd $SCRIPT_PATH/../
 
 PATH_OPTIONS="$REBENCH_CONF_PATH $BENCHS_PATH $RIR_BUILD_PATH $GNU_R_PATH"
-REBENCH_OPTIONS="--commit-id=$COMMIT --branch=$BRANCH --environment=PragueDesktop -df $PERSIST_IN-$TIMESTAMP"
+REBENCH_OPTIONS="-S --commit-id=$COMMIT --branch=$BRANCH --environment=PragueDesktop -df $PERSIST_IN-$TIMESTAMP"
 
 if [[ "$1" == "--docker" ]]; then
     OPTIONS_DOCKER_RIR="$PATH_OPTIONS e:PIR e:RIR $REBENCH_OPTIONS"
     # First use the RIR container to run the benchmarks for RIR and PIR
-    #`docker run -t -v "$ROOT_PATH:$DOCKER_OUT_VOL_NAME" "registry.gitlab.com/rirvm/rir_mirror/benchmark:$COMMIT" /opt/rbenchmarking/Setup/run.sh $OPTIONS_DOCKER_RIR`
+    #echo "docker run --privileged=true -t -v $ROOT_PATH:$DOCKER_OUT_VOL_NAME registry.gitlab.com/rirvm/rir_mirror/benchmark:$COMMIT /opt/rbenchmarking/Setup/run.sh $OPTIONS_DOCKER_RIR"
+    `docker run --privileged=true -v "$ROOT_PATH:$DOCKER_OUT_VOL_NAME" "registry.gitlab.com/rirvm/rir_mirror/benchmark:$COMMIT" /opt/rbenchmarking/Setup/run.sh $OPTIONS_DOCKER_RIR`
     OPTIONS_DOCKER_GNU="$PATH_OPTIONS e:GNU-R $REBENCH_OPTIONS"
     # Then use the GNU-R container to run the benchmarks for GNU-R
-    `docker run -t -v "$ROOT_PATH:$DOCKER_OUT_VOL_NAME" "registry.gitlab.com/rirvm/rir_mirror/benchmark-baseline" /opt/rbenchmarking/Setup/run.sh $OPTIONS_DOCKER_GNU`
+    `docker run --privileged=true -v "$ROOT_PATH:$DOCKER_OUT_VOL_NAME" "registry.gitlab.com/rirvm/rir_mirror/benchmark-baseline" /opt/rbenchmarking/Setup/run.sh $OPTIONS_DOCKER_GNU`
     echo "$COMMIT" > "$DATA_PATH/$LAST_COMMIT_FILENAME"
 else
     OPTIONS_PRG="$PATH_OPTIONS e:GNU-R e:PIR e:RIR $REBENCH_OPTIONS"

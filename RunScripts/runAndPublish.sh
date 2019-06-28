c9437f0cc95ae03ce6c3ebd9c928b8654ed14e1b
oli <o@o1o.ch>
2019-06-28 17:14:42 +0200

    trying to fix benchmark machine

--- RunScripts/runAndPublish.sh
+++ RunScripts/runAndPublish.sh
@@ -41,7 +41,7 @@ pushd $SCRIPT_PATH/../
 if [[ "$1" == "--docker" ]]; then
     echo "docker run --privileged=true -v $ROOT_PATH:$DOCKER_OUT_VOL_NAME registry.gitlab.com/rirvm/rir_mirror/benchmark:master cat $CONTAINER_COMMIT_FILENAME > $DOCKER_DATA_PATH/$CURRENT_COMMIT_FILENAME" 
     COMMAND="cat $CONTAINER_COMMIT_FILENAME > $DOCKER_DATA_PATH/$CURRENT_COMMIT_FILENAME"
-    `docker run --privileged=true -v "$ROOT_PATH:$DOCKER_OUT_VOL_NAME" "registry.gitlab.com/rirvm/rir_mirror/benchmark:master" bash -c "${COMMAND}"`
+    docker run --privileged=true -v "$ROOT_PATH:$DOCKER_OUT_VOL_NAME" "registry.gitlab.com/rirvm/rir_mirror/benchmark:master" bash -c "${COMMAND}"
     COMMIT=$(head -n 1 $DATA_PATH/$CURRENT_COMMIT_FILENAME)
 fi
 
@@ -49,12 +49,10 @@ PATH_OPTIONS="$REBENCH_CONF_PATH $BENCHS_PATH $RIR_BUILD_PATH $GNU_R_PATH $FASTR
 REBENCH_OPTIONS="--commit-id=$COMMIT --branch=$BRANCH --environment=PragueDesktop -df $PERSIST_IN-$TIMESTAMP"
 
 if [[ "$1" == "--docker" ]]; then
-    OPTIONS_DOCKER_RIR="$PATH_OPTIONS \"e:PIR e:RIR $REBENCH_OPTIONS\""
     # First use the RIR container to run the benchmarks for RIR and PIR
-    `docker run --privileged=true -v "$ROOT_PATH:$DOCKER_OUT_VOL_NAME" "registry.gitlab.com/rirvm/rir_mirror/benchmark:master" /opt/rbenchmarking/Setup/run.sh $OPTIONS_DOCKER_RIR`
-    OPTIONS_DOCKER_GNU="$PATH_OPTIONS \"e:GNU-R e:FASTR $REBENCH_OPTIONS\""
+    docker run --privileged=true -v "$ROOT_PATH:$DOCKER_OUT_VOL_NAME" "registry.gitlab.com/rirvm/rir_mirror/benchmark:master" /opt/rbenchmarking/Setup/run.sh $PATH_OPTIONS "e:PIR e:RIR $REBENCH_OPTIONS"
     # Then use the GNU-R container to run the benchmarks for GNU-R
-    `docker run --privileged=true -v "$ROOT_PATH:$DOCKER_OUT_VOL_NAME" "registry.gitlab.com/rirvm/rir_mirror/benchmark-baseline" /opt/rbenchmarking/Setup/run.sh $OPTIONS_DOCKER_GNU`
+    docker run --privileged=true -v "$ROOT_PATH:$DOCKER_OUT_VOL_NAME" "registry.gitlab.com/rirvm/rir_mirror/benchmark-baseline" /opt/rbenchmarking/Setup/run.sh $PATH_OPTIONS "e:GNU-R e:FASTR $REBENCH_OPTIONS"
     echo "$CONTAINER_ID" > "$DATA_PATH/$LAST_CONTAINER_FILENAME"
 else
     OPTIONS_PRG="$PATH_OPTIONS \"e:GNU-R e:PIR e:RIR e:FASTR $REBENCH_OPTIONS\""

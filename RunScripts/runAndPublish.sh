25b5911a5c54065901a8937c1f1c8a760bcac481
Guido Chari <charig@gmail.com>
2019-07-22 10:20:08 +0200

    To send data to speedcenter the REBENCH_ENV var should be set and pointing to a valid environment in speedcenter db

--- RunScripts/runAndPublish.sh
+++ RunScripts/runAndPublish.sh
@@ -32,7 +32,7 @@ COMMIT=$(docker run $RIR_CONTAINER cat /opt/rir_version)
 echo "Commit $COMMIT"
 
 if [[ -z "$REBENCH_ENV" ]]; then
-  echo "Rebench environment missing, results will not be sent to speedcenter"
+  echo "Rebench environment missing. Results will not be sent to speedcenter"
 fi
 
 PATH_OPTIONS="$REBENCH_CONF_PATH $BENCHS_PATH"

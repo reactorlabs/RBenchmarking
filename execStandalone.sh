a75ff89167a25e7b77aad84395cbaee2ec66cb40
oli <o@o1o.ch>
2019-07-24 08:07:08 +0000

    remove unused files

--- execStandalone.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e # make script fail on first error
-systemctl stop gdm
-systemctl stop cron
-systemctl stop ondemand
-while true; do RunScripts/updateRunAndPublish.sh; sleep $((60*10)); done

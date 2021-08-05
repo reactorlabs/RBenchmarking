b5d7d32553605c7e76845344508af22ac7bbf61d
vogr <contact@vogier.fr>
2021-08-05 15:35:09 +0000

    Count number of inner-iterations necessary to reach 200ms of runtime per outer-iteration

---
+++ Setup/genthat/determine_inner_iteration/README.md
@@ -0,0 +1,11 @@
+# Determine how many inner iterations each benchmark should have
+
+## Usage
+
+```
+PATH="PATH_TO_Rsh_bin:$PATH" Rscript min_nb_iter_for_benchmark.R "../../../Benchmarks/genthat-CRAN" > genthat_n_iter.csv
+```
+
+Will generate the file genthat_n_iter.csv containing on each line a test name, and the number of hot iterations of that test necessary to reach a minimum running time (here: 200ms).
+
+Thile will be used when generating the rebench.conf file.
---
+++ Setup/genthat/determine_inner_iteration/min_nb_iter_for_benchmark.R
@@ -0,0 +1,61 @@
+#!/usr/bin/env Rscript
+
+# The loop should take at least MIN_LOOP_TIME and iterate
+# at least MIN_INNER_IT times. This is to make sure that the loop
+# takes long enough to be measured by rebench, and that the warmup
+# happens in the first outer-iteration, no matter how long the
+# inner-iteration is.
+
+MIN_LOOP_TIME = 200 #ms
+MIN_INNER_IT = 10
+
+argv <- commandArgs(trailingOnly=TRUE)
+
+if (length(argv) != 1) {
+  cat("iter_until_time_budget.R benchmark_dir.R\n")
+  cat("\n")
+  stop("Incorrect number of arguments")
+}
+
+BENCHMARK_DIR <- normalizePath(argv[[1]])
+
+required_iterations_for <- function(benchmark) {
+
+  message("Sourcing ", benchmark)
+
+  source(benchmark)
+
+
+
+  # Warmup
+  for (i in 1:MIN_INNER_IT) {
+    function_to_run()
+  }
+
+  # Count number of required interations
+  n_it <- 0
+  t0 <- Sys.time()
+
+  for (i in 1:MIN_INNER_IT) {
+    capture.output(function_to_run())
+    n_it <- n_it + 1
+  }
+  while ( (Sys.time() - t0) * 1000 < MIN_LOOP_TIME) {
+    capture.output(function_to_run())
+    n_it <- n_it + 1
+  }
+
+  n_it
+}
+
+benchmarks <- list.files(BENCHMARK_DIR, "*.R", recursive=TRUE)
+
+# Running the testfiles can create files.
+setwd(tempdir())
+
+for (b in benchmarks) {
+  try({
+    n_it <- required_iterations_for(file.path(BENCHMARK_DIR, b))
+    cat(paste0(b, ",", n_it,"\n"))
+  })
+}

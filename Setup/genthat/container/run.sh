94bdf271e9263f7779dbc9dcf06c77dc4973df0f
vogr <contact@vogier.fr>
2021-08-05 13:08:16 +0000

    Also pin CRAN snapshot for installation of genthat.

--- Setup/genthat/container/run.sh
+++ Setup/genthat/container/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -u
 
+/opt/setup-scripts/install_genthat.R &&
 /opt/setup-scripts/install_pkgs.R /opt/setup-scripts/packages.txt /opt/r_library /opt/cran &&
 /opt/setup-scripts/extract_testcases.R /opt/setup-scripts/packages.txt /opt/r_library /opt/cran /opt/testcases 4
 
--- Setup/genthat/install_genthat.R
+++ Setup/genthat/install_genthat.R
@@ -1,5 +1,8 @@
 #!/usr/bin/env Rscript
 
+# One day before the release of R 3.6.3
+options(repos='https://cran.microsoft.com/snapshot/2020-02-28/')
+
 # remotes will be needed to install `genthat` but does not have to be installed in lib_path
 install.packages("remotes")
 # Install genthat and the dependancies for extract_testcases.R (outside of lib_path)

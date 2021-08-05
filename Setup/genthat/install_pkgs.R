a2bc055546d5c9eac128daf27a22ffc070e8c83c
vogr <contact@vogier.fr>
2021-08-05 09:36:12 +0000

    intall_pkgs.sh: make saving the source optionnal.

--- Setup/genthat/install_pkgs.R
+++ Setup/genthat/install_pkgs.R
@@ -2,24 +2,36 @@
 
 argv <- commandArgs(trailingOnly=TRUE)
 
-if (length(argv) != 2) {
-  message('ERROR: requires two arguments\n')
-  message('install_pkgs.R packages.txt <installation_destination>')
-  stop()
+if (length(argv) < 3 || length(argv) > 4) {
+  message('install_pkgs.R <packages.txt> <pkg_installation_dest_dir> [pkg_source_dest_dir]')
+  stop("Incorrect number of arguments")
+}
+
+mkdir_norm <- function(d) {
+  if(! dir.exists(d)) {
+    dir.create(d)
+  }
+  normalizePath(d)
 }
 
 pkg_file <- normalizePath(argv[[1]])
-lib_path <- argv[[2]]
+lib_path <- mkdir_norm(argv[[2]])
 
-
-if(! dir.exists(lib_path)) {
-  dir.create(lib_path)
+cran_path <- NULL
+if (length(argv) >= 3) {
+  cran_path <- mkdir_norm(argv[[3]])
 }
-lib_path <- normalizePath(lib_path)
 
+gzpath <- tempdir()
 
 # One day before the release of R 3.6.3
 options(repos='https://cran.microsoft.com/snapshot/2020-02-28/')
 
 packages <- scan(pkg_file, character(), sep='\n')
-install.packages(packages, lib=lib_path)
+
+install.packages(packages, lib=lib_path, destdir=gzpath)
+
+for (gzfile in list.files(gzpath, full.names=TRUE)) {
+  untar(gzfile, exdir = cran_path)
+}
+

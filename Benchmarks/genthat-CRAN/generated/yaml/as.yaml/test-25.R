79b6de38651574aef6ea22ce9cc9de8c61c6bcf8
vogr <contact@vogier.fr>
2021-08-06 14:26:17 +0000

    Remove old yaml tests.

--- Benchmarks/genthat-CRAN/generated/yaml/as.yaml/test-25.R
+++
@@ -1,9 +0,0 @@
-library(yaml)
-
-function_to_run <- function() {
-    yaml:::as.yaml(x = 1:10)
-}
-
-
-
-
--- Benchmarks/genthat-CRAN/generated/yaml/yaml.load/test-60.R
+++
@@ -1,9 +0,0 @@
-library(yaml)
-
-function_to_run <- function() {
-    yaml:::yaml.load(string = "foo: 123\nbar: 456")
-}
-
-
-
-
